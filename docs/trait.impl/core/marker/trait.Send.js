fcd5dfb5809e41fe7622ae6d48f8a3bc53905144
flux <49762827+0xflux@users.noreply.github.com>
2024-12-30 17:11:51 +0000

    Update docs for 0.0.4

---
+++ docs/trait.impl/core/marker/trait.Send.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["wdk_mutex",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"enum\" href=\"wdk_mutex/errors/enum.DriverMutexError.html\" title=\"enum wdk_mutex::errors::DriverMutexError\">DriverMutexError</a>",1,["wdk_mutex::errors::DriverMutexError"]],["impl&lt;'a, T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"wdk_mutex/kmutex/struct.KMutexGuard.html\" title=\"struct wdk_mutex::kmutex::KMutexGuard\">KMutexGuard</a>&lt;'a, T&gt;",1,["wdk_mutex::kmutex::KMutexGuard"]],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"wdk_mutex/kmutex/struct.KMutex.html\" title=\"struct wdk_mutex::kmutex::KMutex\">KMutex</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[975]}
