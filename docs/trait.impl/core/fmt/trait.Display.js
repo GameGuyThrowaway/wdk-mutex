4003efbb44a93ba78f3ae0c074d847a5d89b159d
flux <49762827+0xflux@users.noreply.github.com>
2025-10-12 09:57:08 +0100

    Update docs

--- docs/trait.impl/core/fmt/trait.Display.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["wdk_mutex",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"wdk_mutex/fast_mutex/struct.FastMutexGuard.html\" title=\"struct wdk_mutex::fast_mutex::FastMutexGuard\">FastMutexGuard</a>&lt;'_, T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,</div>"],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"wdk_mutex/kmutex/struct.KMutexGuard.html\" title=\"struct wdk_mutex::kmutex::KMutexGuard\">KMutexGuard</a>&lt;'_, T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a>,</div>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[1000]}
